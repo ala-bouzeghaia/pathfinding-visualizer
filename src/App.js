cafb7b73d4a8a99e695193fdec16266aa5655973
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2020-10-13 15:38:19 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>hello</h1>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Navbar = () => {
+    return(
+        <nav className = "navbar navbar-expand-lg">
+            <Link to="/" className="navbar-brand">Pathfinding Visualizer </Link> 
+             
+
+        </nav>
+    );
+};
+
+export default Navbar;
