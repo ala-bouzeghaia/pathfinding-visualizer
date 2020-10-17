083351a7ff59685631af84051466254e03e0c135
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2020-10-17 18:57:50 +0200

    Updated navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,56 +1,64 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return(
-        <nav className = "navbar navbar-expand-lg bg-light">
-            <Link to="/" className="navbar-brand">Pathfinding Visualizer </Link>
-            
-            <div className="collapse navbar-collapse" >
-                <ul className="nav nav-pills">
-                    <li className="nav-item dropdown">
-                    <button className="link-button nav-link dropdown-toggle" 
-                        data-toggle="dropdown"                          
-                        aria-haspopup="true" 
-                        aria-expanded="false">Algorithms</button>
-                    <div className="dropdown-menu">
-                        <button className="dropdown-item" >Action</button>
-                        <button className="dropdown-item" >Another action</button>
-                        <button className="dropdown-item" >Something else here</button>                        
-                    </div>
-                    </li>
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <Link to="/" className="navbar-brand">
+        Pathfinding Visualizer{" "}
+      </Link>
 
-                    <li className="nav-item dropdown">
-                    <button className="link-button nav-link dropdown-toggle" 
-                        data-toggle="dropdown"                          
-                        aria-haspopup="true" 
-                        aria-expanded="false">Mazes</button>
-                    <div className="dropdown-menu">
-                        <button className="dropdown-item" >Action</button>
-                        <button className="dropdown-item" >Another action</button>
-                        <button className="dropdown-item" >Something else here</button>                        
-                    </div>
-                    </li>
+      <div className="collapse navbar-collapse">
+        <ul className="nav nav-pills">
+          <li className="nav-item dropdown">
+            <button
+              className="link-button nav-link dropdown-toggle"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Algorithms
+            </button>
 
-                    <li className="nav-item">
-                        <button className="visualize-button nav-link active">Visualize </button>
-                    </li>
-
-                    <li className="nav-item">
-                        <button className="link-button" >Clear Board </button>
-                    </li>
-
-                    <li className="nav-item">
-                        <button className="link-button" >Clear Path </button>
-                    </li>
-                </ul>                
-                
+            <div className="dropdown-menu">
+              <button className="dropdown-item">Dijkstra's Algorithm</button>
+              <button className="dropdown-item">A* Search</button>
             </div>
-            
-             
+          </li>
 
-        </nav>
-    );
+          <li className="nav-item dropdown">
+            <button
+              className="link-button nav-link dropdown-toggle"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Mazes
+            </button>
+            <div className="dropdown-menu">
+              <button className="dropdown-item">Action</button>
+              <button className="dropdown-item">Another action</button>
+              <button className="dropdown-item">Something else here</button>
+            </div>
+          </li>
+
+          <li className="nav-item">
+            <button className="visualize-button nav-link active">
+              Visualize{" "}
+            </button>
+          </li>
+
+          <li className="nav-item">
+            <button className="link-button">Clear Board </button>
+          </li>
+
+          <li className="nav-item">
+            <button className="link-button">Clear Path </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
