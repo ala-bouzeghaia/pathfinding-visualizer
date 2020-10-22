cf3011e085a7442a43ba68b732ec6d18ff01eb35
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2020-10-22 14:09:40 +0200

    Added maze reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import VisualizeAlgoReducer from "./visualizeAlgo";
 import clearBoardReducer from "./clearboard";
 import clearPathReducer from "./clearpath";
+import MazeReducer from "./mazes";
 import { combineReducers } from "redux";
 
 const allReducers = combineReducers({
   visualizeAlgo: VisualizeAlgoReducer,
   clearBoard: clearBoardReducer,
   clearPath: clearPathReducer,
+  maze: MazeReducer,
 });
 
 export default allReducers;
