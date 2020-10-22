c8b87fc8bef4e6cbf2b50c30263675e132eece4a
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2020-10-22 14:04:11 +0200

    Added maze reducer

---
+++ src/actions/mazes.js
@@ -0,0 +1,17 @@
+export const RandomMaze = () => {
+  return {
+    type: "RANDOM_MAZE",
+  };
+};
+
+export const RecursiveDivision = () => {
+  return {
+    type: "RECURSIVE",
+  };
+};
+
+export const NoMaze = () => {
+  return {
+    type: "NO_MAZE",
+  };
+};
