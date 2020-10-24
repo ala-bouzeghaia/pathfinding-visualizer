671dc31095dddf5358b89e2cf7c3c393b1ac87de
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2020-10-24 19:59:36 +0200

    Added surrounding walls in recursive division

--- src/components/mazes/recursiveDivision.js
+++ src/components/mazes/recursiveDivision.js
@@ -3,10 +3,13 @@ export const recursiveDivision = (grid) => {
   for (let i = 0; i < 20; i++) {
     for (let j = 0; j < 50; j++) {
       const node = newGrid[i][j];
-      if (i === 0 || i === 20) {
+      if (i === 0 || i === 19) {
         const newNode = { ...node, isWall: true };
         newGrid[i][j] = newNode;
-      } else if (i > 0 && i < 20 && (j === 0 || j === 50)) {
+      } else if (
+        (i > 0 && i < 19 && j === 0) ||
+        (i > 0 && i < 19 && j === 49)
+      ) {
         const newNode = { ...node, isWall: true };
         newGrid[i][j] = newNode;
       }
