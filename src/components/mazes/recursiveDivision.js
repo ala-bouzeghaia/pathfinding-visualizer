20511e4acc287cb1baf0914c5d66d3cd5ecc5f20
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2020-10-25 13:44:46 +0100

    Added random wall with hole

--- src/components/mazes/recursiveDivision.js
+++ src/components/mazes/recursiveDivision.js
@@ -1,19 +1,57 @@
 export const recursiveDivision = (grid) => {
   const newGrid = grid.slice();
+  const wall = Math.floor(Math.random() * 18 + 1);
+  const hole = Math.floor(Math.random() * 48 + 1);
   for (let i = 0; i < 20; i++) {
     for (let j = 0; j < 50; j++) {
-      const node = newGrid[i][j];
-      if (i === 0 || i === 19) {
+      //addContours(newGrid, i, j);
+      addWall(newGrid, i, j, wall, hole);
+      //const node = newGrid[i][j];
+
+      /* if (i === wall && j !== hole) {
         const newNode = { ...node, isWall: true };
         newGrid[i][j] = newNode;
-      } else if (
-        (i > 0 && i < 19 && j === 0) ||
-        (i > 0 && i < 19 && j === 49)
-      ) {
+      } */
+      /* if (j !== hole) {
         const newNode = { ...node, isWall: true };
-        newGrid[i][j] = newNode;
-      }
+        newGrid[wall][j] = newNode;
+      } */
     }
   }
   return newGrid;
+};
+
+const addContours = (newGrid, row, col) => {
+  const node = newGrid[row][col];
+  if (row === 0 || row === 19) {
+    const newNode = { ...node, isWall: true };
+    newGrid[row][col] = newNode;
+  } else if (
+    (row > 0 && row < 19 && col === 0) ||
+    (row > 0 && row < 19 && col === 49)
+  ) {
+    const newNode = { ...node, isWall: true };
+    newGrid[row][col] = newNode;
+  }
+};
+
+const addWall = (gridWithWall, row, col, wall, hole) => {
+  const node = gridWithWall[row][col];
+  //const wall = Math.floor(Math.random() * 18 + 1);
+  //const hole = Math.floor(Math.random() * 48 + 1);
+  /* const START_NODE_ROW = 10;
+  const START_NODE_COL = 15;
+  //const FINISH_NODE_ROW = 10;
+  const FINISH_NODE_COL = 35; */
+  /* if (wall === 10) {
+    if (col !== 15 && col !== 35) {
+      //grid[wall][col].isWall = true;
+      const newNode = { ...node, isWall: true };
+      gridWithWall[wall][col] = newNode;
+    }
+  } else */
+  if (row === wall && col !== hole) {
+    const newNode = { ...node, isWall: true };
+    gridWithWall[row][col] = newNode;
+  }
 };
