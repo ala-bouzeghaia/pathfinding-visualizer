f3a247a975d38fe7bec52439b7be24cb6e2810c8
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2020-10-26 19:41:22 +0100

    Changed speed animation for astar

--- src/components/pathvisualizer.js
+++ src/components/pathvisualizer.js
@@ -195,14 +195,14 @@ const PathfindingVisualizer = () => {
       if (i === visitedNodesInOrder.length) {
         setTimeout(() => {
           animateShortestPath(nodesInShortestPathOrder);
-        }, 10 * i);
+        }, 2 * i);
         return;
       }
       setTimeout(() => {
         const node = visitedNodesInOrder[i];
         document.getElementById(`node-${node.row}-${node.col}`).className =
           "node node-visited";
-      }, 10 * i);
+      }, 2 * i);
     }
   };
 
