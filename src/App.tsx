937f23b5e45ee45943fd66d3756f2bbf7921bdbe
Ala Bouzeghaia <ala.bouzeghaia@gmail.com>
2025-01-15 21:46:19 +0100

    feat: Implement grid layout with dynamic resizing and initial grid setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,52 @@
+import { useEffect, useRef, useState } from "react";
+import { Navbar } from "./components/Navbar";
+import { Grid } from "./components/Grid";
+import { NodeType } from "./types/node.type";
+
+import { NUMBER_OF_COLS, NUMBER_OF_ROWS, SQUARE_SIZE } from "./utils/const";
+import { getInitialGrid } from "./utils/grid";
+
 export function App() {
+  const [algo, setAlgo] = useState("");
+  console.log(algo);
+  const gridRef = useRef<HTMLDivElement>(null);
+
+  const [gridSize, setGridSize] = useState({
+    rows: NUMBER_OF_ROWS,
+    cols: NUMBER_OF_COLS,
+  });
+  const { rows: numRows, cols: numCols } = gridSize;
+
+  const initialGrid = getInitialGrid(numRows, numCols);
+  const [grid, setGrid] = useState<NodeType[][]>(initialGrid);
+
+  function handleResize() {
+    if (gridRef.current) {
+      setGridSize({
+        rows: Math.floor(gridRef?.current.offsetHeight / SQUARE_SIZE),
+        cols: Math.floor(gridRef?.current.offsetWidth / SQUARE_SIZE),
+      });
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div className='flex flex-col w-full h-screen gap-4'>
+      <header className='w-full h-8vh bg-amber-100 text-gray-700'>
+        <Navbar setAlgo={setAlgo} />
+      </header>
+
+      <main className='flex mx-auto w-full max-w-1000px h-[92vh]'>
+        <aside className='w-15% flex flex-col gap-4'>
+          <h1 className='text-2xl font-semibold'>Actions</h1>
   return <div>Hello</div>;
+        </aside>
+        <Grid gridRef={gridRef} grid={grid} />
+      </main>
+    </div>
+  );
 }
---
+++ src/utils/grid.ts
@@ -0,0 +1,32 @@
+import { NodeType } from "../types/node.type";
+import {
+  START_NODE_COL,
+  START_NODE_ROW,
+  FINISH_NODE_COL,
+  FINISH_NODE_ROW,
+} from "./const";
+
+function createNode(row: number, col: number): NodeType {
+  return {
+    row,
+    col,
+    isStart: row === START_NODE_ROW && col === START_NODE_COL,
+    isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
+    distance: Infinity,
+    isVisited: false,
+    isWall: false,
+    previousNode: null,
+  };
+}
+
+export function getInitialGrid(numRows: number, numCols: number): NodeType[][] {
+  const grid = [];
+  for (let row = 0; row < numRows; row++) {
+    const currentRow = [];
+    for (let col = 0; col < numCols; col++) {
+      currentRow.push(createNode(row, col));
+    }
+    grid.push(currentRow);
+  }
+  return grid;
+}
