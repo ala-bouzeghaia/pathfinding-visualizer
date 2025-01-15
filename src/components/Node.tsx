51228fdcc52543f293aa48facca7bd5f8240fe11
Ala Bouzeghaia <ala.bouzeghaia@gmail.com>
2025-01-15 22:40:39 +0100

    feat: Add reset wall animation

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -12,7 +12,9 @@ export function Node(props: Props) {
   return (
     <div
       className={`w-30px h-30px border-1 border-blue-500 flex items-center justify-center ${
-        isWall && "animate-wallAnimation bg-[rgb(12,53,71)]"
+        isWall
+          ? "animate-wallAnimation bg-[rgb(12,53,71)]"
+          : "animate-resetWallAnimation"
       }`}>
       {isStart && <div className='i-lucide:target w-7 h-7 bg-purple-700' />}
       {isFinish && <div className='i-lucide:circle-x w-7 h-7 bg-purple-700' />}
--- uno.config.ts
+++ uno.config.ts
@@ -23,28 +23,38 @@ export default defineConfig({
     animation: {
       keyframes: {
         wallAnimation:
-          "{0% { transform: scale(0.3); } 50% { transform: scale(1.2); } 100% { transform: scale(1); } 0%, 50%, 100% {background-color: rgb(12, 53, 71); outline: 1px solid rgb(12, 53, 71); }}",
-        duration: {
-          wallAnimation: "0.3s",
-        },
-        timingFunction: {
-          wallAnimation: "ease-out",
-        },
-        delay: {
-          wallAnimation: "0s",
-        },
-        iterationCount: {
-          wallAnimation: "1",
-        },
-        direction: {
-          wallAnimation: "alternate",
-        },
-        fillMode: {
-          wallAnimation: "forwards",
-        },
-        playState: {
-          wallAnimation: "running",
-        },
+          "{0% { transform: scale(0.3); } 50% { transform: scale(1.1); } 100% { transform: scale(1); } 0%, 50%, 100% {background-color: rgb(12, 53, 71); outline: 1px solid rgb(12, 53, 71); }}",
+        resetWallAnimation:
+          "{0% { transform: scale(1); } 25% { transform: scale(0.5); } 100% { transform: scale(0); } 0%, 25%, 100% {background-color: rgb(12, 53, 71); outline: 1px solid rgb(12, 53, 71); }}",
+      },
+
+      durations: {
+        wallAnimation: "0.3s",
+        resetWallAnimation: "0.2s",
+      },
+      timingFunction: {
+        wallAnimation: "ease-out",
+        resetWallAnimation: "ease-out",
+      },
+      delay: {
+        wallAnimation: "0s",
+        resetWallAnimation: "0s",
+      },
+      iterationCount: {
+        wallAnimation: "1",
+        resetWallAnimation: "1",
+      },
+      direction: {
+        wallAnimation: "alternate",
+        resetWallAnimation: "alternate",
+      },
+      fillMode: {
+        wallAnimation: "forwards",
+        resetWallAnimation: "forwards",
+      },
+      playState: {
+        wallAnimation: "running",
+        resetWallAnimation: "running",
       },
     },
   },
