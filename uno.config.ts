45bef1fb8edc79bf63400aa32e947df4e9c3f939
Ala Bouzeghaia <ala.bouzeghaia@gmail.com>
2025-01-15 17:49:25 +0100

    feat: Add custom animation theme to UnoCSS config

--- uno.config.ts
+++ uno.config.ts
@@ -19,4 +19,33 @@ export default defineConfig({
   content: {
     filesystem: ["src/**/*"],
   },
+  theme: {
+    animation: {
+      keyframes: {
+        wallAnimation:
+          "{0% { transform: scale(0.3); } 50% { transform: scale(1.2); } 100% { transform: scale(1); } 0%, 50%, 100% {background-color: rgb(12, 53, 71); outline: 1px solid rgb(12, 53, 71); }}",
+        duration: {
+          wallAnimation: "0.3s",
+        },
+        timingFunction: {
+          wallAnimation: "ease-out",
+        },
+        delay: {
+          wallAnimation: "0s",
+        },
+        iterationCount: {
+          wallAnimation: "1",
+        },
+        direction: {
+          wallAnimation: "alternate",
+        },
+        fillMode: {
+          wallAnimation: "forwards",
+        },
+        playState: {
+          wallAnimation: "running",
+        },
+      },
+    },
+  },
 });
