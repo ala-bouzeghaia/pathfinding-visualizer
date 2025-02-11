d57063e8442c3c5716419a1ce5ac994fbb8c2f06
Ala Bouzeghaia <ala.bouzeghaia@gmail.com>
2025-02-11 13:14:01 +0100

    feat :Add node animations for walls, visited nodes, and shortest path

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -2,22 +2,41 @@ import type { NodeType } from "../types/node.type";
 
 type Props = {
   node: NodeType;
+  animatedWallNodes: Set<string>;
+  animatedVisitedNodes: Set<string>;
+  animatedShortestPathNodes: Set<string>;
 };
 
 export function Node(props: Props) {
   const {
-    node: { isStart, isFinish, isWall },
+    node,
+    animatedWallNodes,
+    animatedVisitedNodes,
+    animatedShortestPathNodes,
   } = props;
 
   return (
     <div
+      id={`node-${node.row}-${node.col}`}
       className={`w-30px h-30px border-1 border-blue-500 flex items-center justify-center ${
-        isWall
-          ? "animate-wallAnimation bg-[rgb(12,53,71)]"
-          : "animate-resetWallAnimation"
+        animatedWallNodes.has(`node-${node.row}-${node.col}`)
+          ? "animate-wallAnimation"
+          : ""
+      } ${
+        animatedVisitedNodes.has(`node-${node.row}-${node.col}`)
+          ? "animate-visitedAnimation"
+          : ""
+      } ${
+        animatedShortestPathNodes.has(`node-${node.row}-${node.col}`)
+          ? "animate-pathAnimation"
+          : ""
       }`}>
-      {isStart && <div className='i-lucide:target w-7 h-7 bg-purple-700' />}
-      {isFinish && <div className='i-lucide:circle-x w-7 h-7 bg-purple-700' />}
+      {node.isStart && (
+        <div className='i-lucide:target w-7 h-7 bg-purple-700' />
+      )}
+      {node.isFinish && (
+        <div className='i-lucide:circle-x w-7 h-7 bg-purple-700' />
+      )}
     </div>
   );
 }
