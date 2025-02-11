17ef7e64273cb985c8d14986fa2a086ce8391553
Ala Bouzeghaia <ala.bouzeghaia@gmail.com>
2025-02-11 13:25:07 +0100

    feat: Remove unused CSS files

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import "./index.css";
 import "virtual:uno.css";
 import "@unocss/reset/tailwind.css";
 import { App } from "./App.tsx";
