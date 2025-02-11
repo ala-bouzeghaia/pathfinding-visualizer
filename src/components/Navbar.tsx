0e9b868dbe70ca3d4f0956ad46f9fe331615c35d
Ala Bouzeghaia <ala.bouzeghaia@gmail.com>
2025-02-11 13:12:39 +0100

    feat: Update Navbar component for mobile menu functionality and styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,23 @@
 type Props = {
-  setAlgo: (value: string) => void;
+  openMobileMenu: boolean;
+  setOpenMobileMenu: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 export function Navbar(props: Props) {
-  const { setAlgo } = props;
+  const { openMobileMenu, setOpenMobileMenu } = props;
 
   return (
-    <nav className='h-full w-full max-w-[1000px] my-0 mx-auto flex justify-between items-center p-4'>
-      <h1 className='text-2xl font-bold'>Logo</h1>
-      <ul className='flex space-x-4'>
-        <li>
-          <button onClick={() => setAlgo("Home")}>Home</button>
-        </li>
-        <li>
-          <button onClick={() => setAlgo("About")}>About</button>
-        </li>
-        <li>
-          <button onClick={() => setAlgo("Contact")}>Contact</button>{" "}
-        </li>
-      </ul>
+    <nav className='h-full my-0 mx-auto flex justify-between items-center p-4'>
+      <h1 className='text-xl sm:text-2xl font-bold'>Pathfinding Visualizer</h1>
+      <div
+        className='space-y-2 cursor-pointer sm:(hidden)'
+        onClick={() => setOpenMobileMenu(!openMobileMenu)}>
+        {openMobileMenu ? (
+          <div className='i-lucide:x h-6 w-6' />
+        ) : (
+          <div className='i-lucide:menu h-6 w-6' />
+        )}
+      </div>
     </nav>
   );
 }
